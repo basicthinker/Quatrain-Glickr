d0e58d7fb6239f361b8f7d63014e4dba417b47fd
Santa Zhang <santa1987@gmail.com>
2011-03-15 23:09:15 +0800

    added timing for several group search test cases

--- src/org/stanzax/glickr/Application.java
+++ src/org/stanzax/glickr/Application.java
@@ -1,16 +1,28 @@
 package org.stanzax.glickr;
 
+import java.util.Date;
+
 public class Application {
-	
-	public static void main(String[] args) {
-		System.out.println("This is Glickr!");
-		String keywords = "cat";
-		String groupSearch = "usaunitedstatesofamerica";
+
+	private static void queryWithTiming(String keywords, String groupName) {
 		int beginNum = 0;
 		int endNum = 19;
-		GlickrItem items[] = GlickrItem.getGlickrItems(keywords, groupSearch, beginNum, endNum);
-		for (GlickrItem item : items) {
-			System.out.println(item);
+		Date startTime = new Date();
+		GlickrItem.getGlickrItems(keywords, groupName, beginNum, endNum);
+		Date stopTime = new Date();
+		long milli = stopTime.getTime() - startTime.getTime();
+		System.out.println("finished querying \"" + keywords + "\" in group \""
+				+ groupName + "\", time cost is " + milli + " ms");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("This is Glickr!");
+		String[] groups = { "china", "usaunitedstatesofamerica", "japan",
+				"australia", "canada", "norway", "england", "france", "egypt",
+				"south_africa", "madagascar" };
+		String keywords = "cat";
+		for (String group : groups) {
+			queryWithTiming(keywords, group);
 		}
 	}
 
